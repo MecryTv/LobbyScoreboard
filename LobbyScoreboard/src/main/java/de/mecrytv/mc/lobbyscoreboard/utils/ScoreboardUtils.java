51971c12091fc59c16aa4b72f8b2dfdd30ecdcd9
MecryTv <125377912+MecryTv@users.noreply.github.com>
2023-05-20 00:36:46 +0200

    Add files via upload

--- LobbyScoreboard/src/main/java/de/mecrytv/mc/lobbyscoreboard/utils/ScoreboardUtils.java
+++ LobbyScoreboard/src/main/java/de/mecrytv/mc/lobbyscoreboard/utils/ScoreboardUtils.java
@@ -24,26 +24,35 @@ public class ScoreboardUtils {
         String prefix = user.getCachedData().getMetaData().getPrefix();
         String currentWorld = player.getWorld().getName();
 
-        objective.getScore("Test").setScore(0);
-        objective.getScore("§a").setScore(1);
-        objective.getScore(currentWorld).setScore(2);
-        objective.getScore("Dein Server").setScore(3);
-        objective.getScore("§a").setScore(4);
-        objective.getScore(prefix).setScore(5);
-        objective.getScore("Dein Rank").setScore(6);
-        objective.getScore("§a").setScore(7);
+        objective.getScore("§9§l§r§9■■■■■■■■■■").setScore(0);
+        objective.getScore(currentWorld).setScore(1);
+        objective.getScore("§6➜" + " " + "§3Server").setScore(2);
+        objective.getScore("§c").setScore(3);
+        objective.getScore("CoinsAPI").setScore(4);
+        objective.getScore("§6➜" + " " + "§3Coins").setScore(5);
+        objective.getScore("§e").setScore(6);
+        objective.getScore("§7" + Bukkit.getOnlinePlayers().size()  + "§b/" + "§7" + Bukkit.getMaxPlayers()).setScore(7);
+        objective.getScore("§6➜" + " " + "§3Online").setScore(8);
+        objective.getScore("§f").setScore(9);
+        objective.getScore(prefix).setScore(10);
+        objective.getScore("§6➜" + " " + "§3Rank").setScore(11);
+        objective.getScore("§9■■■■■■■■■■").setScore(12);
 
 
 
-
-        //-             7
-        //Dein Rank:    6
-        //            / 5
-        //-             4
-        //Dein Server:  3
-        //              2
-        //-             1
-        //              0
+        //-       12
+        //Rank    11
+        //        10
+        //-       9
+        //Online: 8
+        //        7
+        //-       6
+        //Coins:  5
+        //        4
+        //-       3
+        //Server: 2
+        //        1
+        //-       0
 
         return s;
     }
